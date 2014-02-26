c35ac8a66cb9678f01324f74be0eeea9ec31334f
SireMartin <maarten.deheegher@gmail.com>
2014-02-26 21:45:03 +0000

    write facts to db

--- eventLogger.cpp
+++ eventLogger.cpp
@@ -32,6 +32,7 @@ union UFloat
   unsigned char bytes[4];
 };
 
+//global variables
 unsigned char checksum;
 
 unsigned char getByte(bool updateChecksum = true)
@@ -96,6 +97,7 @@ void loop()
 
         //substract msglength with 64, 16 bit address and receive options
         unsigned char payload[msgLength.value - 12];
+        printf("payload length = %d\n", msgLength.value - 12);
         for(int i = 0; i < msgLength.value - 12; ++i)
         {
           payload[i] = getByte();
@@ -107,7 +109,7 @@ void loop()
         {
           //msg ok : parse payload
           UFloat temperature, milliVolt;
-          UShort device;
+          unsigned char device;
           temperature.bytes[0] = payload[0];
           temperature.bytes[1] = payload[1];
           temperature.bytes[2] = payload[2];
@@ -116,20 +118,48 @@ void loop()
           milliVolt.bytes[1] = payload[5];
           milliVolt.bytes[2] = payload[6];
           milliVolt.bytes[3] = payload[7];
-          device.bytes[0] = payload[8];
-          device.bytes[1] = payload[9];
-          printf("device = %d / temp = %f / mV = %f\n", device.value, temperature.value, milliVolt.value);
+          device = payload[8];
+          printf("device = %hhu / temp = %f / mV = %f\n", device, temperature.value, milliVolt.value);
 
           //write to db
           //-----------
-          time_t currentTime = time(NULL);
+
+          /*time_t currentTime = time(NULL);
           tm *localTime = localtime(&currentTime);
           char strLocalTime[20];
           strftime(strLocalTime, 20, "%Y-%m-%d %H:%M:%S", localTime);
-          printf("%s\n", strLocalTime);
+          printf("%s\n", strLocalTime);*/
+
+          //connect to db
+          MYSQL *pConn = mysql_init(NULL);
+          if(!pConn)
+          {
+            printf("Unable to init DB connection, give up and try next iteration\n");
+            return;
+          }
+          if(!mysql_real_connect(pConn, "localhost", "eventlogger", "***", "events", 0, NULL, 0))
+          {
+            printf("Unable connect to DB, give up and try next iteration\n");
+            return;
+          }
           //construct sql query
           char sqlStmt[200];
-          //sprintf(sqlStmt, "insert into ",);
+          sprintf(sqlStmt, "insert into events(msg_nr, mod_timestamp, data_seq, data_type, data) values (3, now(), 1, 'F', %f)", temperature.value);
+          if(mysql_query(pConn, sqlStmt))
+          {
+            printf("Failed to insert values into table events, try next one...\n");
+          }
+          sprintf(sqlStmt, "insert into events(msg_nr, mod_timestamp, data_seq, data_type, data) values (3, now(), 2, 'F', %f)", milliVolt.value);
+          if(mysql_query(pConn, sqlStmt))
+          {
+            printf("Failed to insert values into table events, try next one...\n");
+          }
+          sprintf(sqlStmt, "insert into events(msg_nr, mod_timestamp, data_seq, data_type, data) values (3, now(), 3, 'I', %hhu)", device);
+          if(mysql_query(pConn, sqlStmt))
+          {
+            printf("Failed to insert values into table events, try next one...\n");
+          }
+          mysql_close(pConn);
         }
       }
     }
