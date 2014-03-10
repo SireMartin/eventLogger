4a8589d2a60cc7d0d06c5153e6ba263b97a9299e
SireMartin <maarten.deheegher@gmail.com>
2014-03-10 22:33:43 +0000

    deur kot state loggen

--- eventLogger.cpp
+++ eventLogger.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include <stdio.h>
 #include <unistd.h>
 #include <my_global.h>
@@ -35,10 +36,13 @@ union UFloat
 
 //global variables
 unsigned char checksum;
+FILE *pOut;
 
 unsigned char getByte(bool updateChecksum = true)
 {
   unsigned char buffer = Serial.read();
+  if(pOut != NULL)
+    fputc(buffer, pOut);
   if(0x7E == buffer)
   {
     checksum = 0;
@@ -46,23 +50,28 @@ unsigned char getByte(bool updateChecksum = true)
   else if(0x7D == buffer)
   {
     buffer = Serial.read();
+    if(pOut != NULL)
+      fputc(buffer, pOut);
     buffer ^= 0x20;
   }
   if(updateChecksum)
   {
     checksum += buffer;
   }
+  fflush(pOut);
   return buffer;
 }
 
 void setup()
 {
   Serial.begin(9600);
+  pOut = fopen("/home/pi/projects/eventLogger/out.txt", "w");
 }
 
 void loop()
 {
   //printf("MySQL client version: %s\n", mysql_get_client_info());
+  //printf("%d\n", Serial.available());
   while(Serial.available())
   {
     if(0x7E == getByte(false))
@@ -74,7 +83,7 @@ void loop()
       msgLength.bytes[0] = lengthLSB;
 
       unsigned char frameType = getByte();
-      printf("Incoming msg frameType 0x%X and length %d bytes\n", frameType, msgLength.value);
+      //printf("Incoming msg frameType 0x%X and length %d bytes\n", frameType, msgLength.value);
 
       if(0x90 == frameType)
       {
@@ -87,12 +96,12 @@ void loop()
         {
           senderL.bytes[i] = getByte();
         }
-        printf("sender 64 bit address H = %X L = %X\n", senderH.value, senderL.value);
+        //printf("sender 64 bit address H = %X L = %X\n", senderH.value, senderL.value);
 
         UShort senderMY;
         senderMY.bytes[1] = getByte();
         senderMY.bytes[0] = getByte();
-        printf("sender 16 bit address = %X\n", senderMY.value);
+        //printf("sender 16 bit address = %X\n", senderMY.value);
 
         unsigned char receiveOptions = getByte();
 
@@ -110,26 +119,26 @@ void loop()
         {
           //msg ok : parse payload
           UFloat temperature, milliVolt;
-          unsigned char device;
-          temperature.bytes[0] = payload[0];
-          temperature.bytes[1] = payload[1];
-          temperature.bytes[2] = payload[2];
-          temperature.bytes[3] = payload[3];
-          milliVolt.bytes[0] = payload[4];
-          milliVolt.bytes[1] = payload[5];
-          milliVolt.bytes[2] = payload[6];
-          milliVolt.bytes[3] = payload[7];
-          device = payload[8];
-          printf("device = %hhu / temp = %f / mV = %f\n", device, temperature.value, milliVolt.value);
-
-          //write to db
-          //-----------
-
-          /*time_t currentTime = time(NULL);
-          tm *localTime = localtime(&currentTime);
-          char strLocalTime[20];
-          strftime(strLocalTime, 20, "%Y-%m-%d %H:%M:%S", localTime);
-          printf("%s\n", strLocalTime);*/
+          unsigned char device, eventType, objectState;
+          eventType = payload[0];
+          if(eventType == 0x1)
+          {
+            temperature.bytes[0] = payload[1];
+            temperature.bytes[1] = payload[2];
+            temperature.bytes[2] = payload[3];
+            temperature.bytes[3] = payload[4];
+            milliVolt.bytes[0] = payload[5];
+            milliVolt.bytes[1] = payload[6];
+            milliVolt.bytes[2] = payload[7];
+            milliVolt.bytes[3] = payload[8];
+            device = payload[9];
+            printf("device = %hhu / temp = %f / mV = %f\n", device, temperature.value, milliVolt.value);
+          }
+          else if(eventType == 0x2)
+          {
+			objectState = payload[1];
+			printf("deur kot state = %hhd\n", objectState);
+		  }
 
           //connect to db
           MYSQL *pConn = mysql_init(NULL);
@@ -145,7 +154,14 @@ void loop()
           }
           //construct sql query
           char sqlStmt[200];
-          sprintf(sqlStmt, "insert into eventLog(event_type, value_int1, value_float1, value_float2, mod_timestamp) values (1, %hhu, %f, %f, now())", device, milliVolt.value, temperature.value);
+          if(eventType == 0x1)	//periodieke meetwaarden
+          {
+			sprintf(sqlStmt, "insert into eventLog(event_type, value_int1, value_float1, value_float2, mod_timestamp) values (%hhu, %hhu, %f, %f, now())", eventType, device, milliVolt.value, temperature.value);
+		  }
+		  else if(eventType == 0x2)	//deur kot state changed
+		  {
+		    sprintf(sqlStmt, "insert into eventLog(event_type, value_int1, mod_timestamp) values (%hhu, %hhu, now())", eventType, objectState);  
+		  }
           if(mysql_query(pConn, sqlStmt))
           {
             printf("Failed to insert values into table events, try next one...\n");
@@ -153,10 +169,10 @@ void loop()
           mysql_close(pConn);
         }
       }
-      std::cout << std::endl;
+      //std::cout << std::endl;
     }
   }
-  usleep(10000);
+  usleep(1000000);
 }
 
 int main ()
